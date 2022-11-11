c8b4d2099af63656392ecb4b4d261d236f77daea
Mehedi Hasan <117031305+mehediHasanAna@users.noreply.github.com>
2022-11-11 23:10:27 +0600

    Add files via upload

--- First.c
+++ First.c
@@ -1,8 +1,32 @@
-#include <stdio.h>
-main(){
-    int a,b,c;
-    a = 5;
-    a += 10;
-    c = a + 3;
-    printf("c = %d\n",c);
-}
+
+
+
+
+
+
+
+
+
+// Lecture 9 . HSC preparation
+// #include <stdio.h>
+// int main(){
+//    int marks;
+//    scanf("%d", &marks);
+//    if(marks >= 101){
+//    printf("Your data invalid\n");
+//    }else if(marks >= 80){
+//    printf("Your grande is A+\n");
+//    }else if(marks >= 70){
+//        printf("Your grande is A\n");
+//    }else if(marks >= 60){
+//        printf("Your grande is A-\n");
+//    }else if(marks >= 50){
+//        printf("Your grande is B\n");
+//    }else if(marks >= 40){
+//        printf("Your grande is C\n");
+//    }else if(marks >= 33){
+//        printf("Your grande is D\n");
+//    }else{
+//        printf("Your grande is F\n");
+//    }
+// }
