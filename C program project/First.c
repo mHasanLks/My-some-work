d2274bac758a526bcb07c14da32b942abb96ca6f
Mehedi Hasan <117031305+mehediHasanAna@users.noreply.github.com>
2022-11-13 19:11:41 +0600

    C program class test

---
+++ C program project/First.c
@@ -0,0 +1,71 @@
+// #include <stdio.h>
+// int main(){
+// int number;
+// scanf("%d", &number);
+// if(number > 0){
+//     printf("Number is Posetive");
+// }else{
+// printf("Number is Negetive");
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+// int main(){
+// int number;
+// scanf("%d", &number);
+// if(number % 2 == 0){
+//     printf("is a Even Number.\n");
+// }else{
+// printf("is a Odd Number.\n");
+// }
+// };
+
+
+// #include <stdio.h>
+// int main(){
+//  int age;
+//  scanf("%d", &age);
+//  if(age >= 18 && age <= 35){
+//     printf("Yes, you are eligible\n");
+//  }else{
+//  printf("sorry! you are not eliginble\n");
+//  }
+// };
+
+
+// Lecture 9 . HSC preparation
+// #include <stdio.h>
+// int main(){
+//    int marks;
+//    scanf("%d", &marks);
+//    if(marks >= 101){
+//    printf("Your data invalid\n");
+//    }else if(marks >= 80){
+//    printf("Your grande is A+\n");
+//    }else if(marks >= 70){
+//        printf("Your grande is A\n");
+//    }else if(marks >= 60){
+//        printf("Your grande is A-\n");
+//    }else if(marks >= 50){
+//        printf("Your grande is B\n");
+//    }else if(marks >= 40){
+//        printf("Your grande is C\n");
+//    }else if(marks >= 33){
+//        printf("Your grande is D\n");
+//    }else{
+//        printf("Your grande is F\n");
+//    }
+// }
