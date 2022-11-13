78b0e452a66222fad435c2ec47714b6478e18f33
Mehedi Hasan <117031305+mehediHasanAna@users.noreply.github.com>
2022-11-13 20:58:01 +0600

    Add files via upload

--- First.c
+++ First.c
@@ -1,10 +1,110 @@
+// #include <stdio.h>
+// int main(){
+// int n, sum;
+// sum = 0;
+// n = 1;
+// while(n <= 10000){
+//     sum = sum + n;
+//     n = n +1;
+// }
+// printf("%d\n",sum);
+// }
+
+
+// #include <stdio.h>
+// int main(){
+// int n;
+// n = 2;
+// while(n <= 100){
+//         printf("%d\n", n);
+//     n = n + 2;
+// }
+// }
+
+// OR
+
+
+// #include <stdio.h>
+// int main(){
+// int n;
+// n = 1;
+// while(n <= 100){
+//     if(n % 2 == 0){
+//         printf("%d\n", n);
+//     }
+//     n = n + 1;
+// }
+// }
 
 
 
 
 
 
+//#include <stdio.h>
+//int main(){
+//int i;
+//i = 0;
+//while (i < 1000){
+   // printf("%d I Love Bangladesh.\n",i);
+   // i = i + 1;
+ //  printf("%d\n", i);
+ //  i++;
 
+//}
+//}
+
+
+// #include <stdio.h>
+// int main(){
+// int num;
+// scanf("%d", &num);
+//  if(num % 3 ==0 && num % 5 ==0){
+//     printf("FizzBuzz.\n");
+//  }else if(num % 3 == 0){
+//  printf("Fizz.\n");
+//  }else if(num % 5 == 0){
+//  printf("Buzz.\n");
+//  }else{
+//  printf("This number not for it");
+//  }
+// }
+
+// #include <stdio.h>
+// int main(){
+// int number;
+// scanf("%d", &number);
+// if(number > 0){
+//     printf("Number is Posetive");
+// }else{
+// printf("Number is Negetive");
+// }
+// }
+
+
+
+// #include <stdio.h>
+// int main(){
+// int number;
+// scanf("%d", &number);
+// if(number % 2 == 0){
+//     printf("is a Even Number.\n");
+// }else{
+// printf("is a Odd Number.\n");
+// }
+// };
+
+
+// #include <stdio.h>
+// int main(){
+//  int age;
+//  scanf("%d", &age);
+//  if(age >= 18 && age <= 35){
+//     printf("Yes, you are eligible\n");
+//  }else{
+//  printf("sorry! you are not eliginble\n");
+//  }
+// };
 
 
 // Lecture 9 . HSC preparation
